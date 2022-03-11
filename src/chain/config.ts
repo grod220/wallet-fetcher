17b25a06dd2a103aafb23f05dedd4e51c2928f88
Gabe Rodriguez <grod220@gmail.com>
2022-03-11 09:16:53 +0100

    Migrating to Alchemy

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,12 +1,9 @@
 import { ethers } from 'ethers';
 import { guardianContractAbi } from './guardianContractAbi';
 
-const NETWORK = 'mainnet';
+const NETWORK = 'homestead';
 
-export const provider = new ethers.providers.InfuraProvider(NETWORK, {
-  projectId: process.env.INFURA_PROJECT_ID,
-  projectSecret: process.env.INFURA_PROJECT_SECRET,
-});
+export const provider = new ethers.providers.AlchemyProvider(NETWORK, process.env.ALCHEMY_API_KEY);
 
 export const guardianContract = new ethers.Contract(
   '0xFF5A7299ff6f0fbAad9b38906b77d08c0FBdc9A7',
